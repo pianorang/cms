dfd45c0d53eb4fe116e57fa901fa76aa444c6966
DESKTOP-HMRF44O\intellian <sunghan.jeon@intelliansys.co.kr>
2022-03-10 18:43:36 +0900

    spring security 설정

---
+++ src/main/java/me/pianorang/cms/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package me.pianorang.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin();
+        return http.build();
+    }
+
+    @Configuration
+    public class SecurityConfiguration {
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return (web) -> web.ignoring().antMatchers("/ignore2");
+        }
+
+    }
+}
--- src/main/java/me/pianorang/cms/member/adapter/in/web/MemberListController.java
+++ src/main/java/me/pianorang/cms/member/adapter/in/web/MemberListController.java
@@ -1,7 +1,14 @@
 package me.pianorang.cms.member.adapter.in.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberListController {
+
+    @GetMapping("/member/list")
+    public String lists(){
+        return "member/list";
+    }
 }
